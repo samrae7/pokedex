1ba8c523f1f10a041074b0d6ab738a2fca8a66f3
Samuel Rae <samovar@pop-os.localdomain>
2020-08-23 18:02:26 +1200

    add Pokedex

---
+++ src/Pokedex/Pokedex.js
@@ -0,0 +1,32 @@
+import React, { useEffect, useState } from "react";
+
+export default function Pokedex() {
+  const [pokemon, setPokemon] = useState([]);
+
+  useEffect(() => {
+    async function fetchPokemon() {
+      try {
+        const response = await fetch(
+          "https://pokeapi.co/api/v2/pokemon?limit=150"
+        );
+        const { results } = await response.json();
+        setPokemon(results);
+      } catch {
+        return null;
+      }
+    }
+
+    fetchPokemon();
+  }, []);
+
+  return (
+    <>
+      <h1>My Pokedex</h1>
+      <ul>
+        {pokemon.map((poke, i) => (
+          <li key={`poke-${i}`}>{poke.name}</li>
+        ))}
+      </ul>
+    </>
+  );
+}
---
+++ src/Pokedex/Pokedex.test.js
@@ -0,0 +1,36 @@
+import React from "react";
+import { render, screen, act } from "@testing-library/react";
+import Pokedex from "./Pokedex";
+
+describe("Pokedex", () => {
+  afterAll(() => {
+    fetch.resetMocks();
+  });
+
+  beforeAll(() => {
+    fetch.mockResponse(
+      JSON.stringify({
+        results: [
+          { name: "bulbasaur", url: "acme.co.nz" },
+          { name: "ivysaur", url: "acme.co.nz" },
+        ],
+      })
+    );
+  });
+
+  it("should display the correct heading", async () => {
+    act(() => {
+      render(<Pokedex />);
+    });
+    expect(await screen.findByText("My Pokedex")).toBeInTheDocument();
+  });
+
+  it("should display a list of Pokemon", async () => {
+    act(() => {
+      render(<Pokedex />);
+    });
+    expect(await screen.findAllByRole("listitem")).toHaveLength(2);
+    expect(await screen.findByText("bulbasaur")).toBeInTheDocument();
+    expect(await screen.findByText("ivysaur")).toBeInTheDocument();
+  });
+});
